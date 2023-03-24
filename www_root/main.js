2253aca380512daf2bd6af4e98481c80573a6a09
Jonathan Lake <jonlake5@gmail.com>
2023-03-24 17:23:15 -0400

    Clean up commented code

--- www_root/main.js
+++ www_root/main.js
@@ -5,9 +5,6 @@ const getUrlUriEndoint = uriBase + 'get_url';
 const authUrl = 'https://auth.jlake.aws.sentinel.com/login?client_id=fpmf66abi4079ea7m5ecbl5fo&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fapp.jlake.aws.sentinel.com'
 
 //Verify this person is authenticated
-// const url = window.location.href;
-// const urlParams = new URLSearchParams(queryString);
-// let urlString = getAllUrlParams(queryString);
 const id_token = idToken(window.location.href);
 if (! id_token) {
     console.log("Not seeing id_token in params")
@@ -43,7 +40,6 @@ $('#to_date').on('change', function() {
 });
 
 function idToken(url) {
-    // let queryString = url ? url.split('#')[1] : window.location.search.slice(1);
     let queryString = url.split('#')[1]
     if (queryString) {
         params = queryString.split('&')
@@ -57,71 +53,6 @@ function idToken(url) {
         }
     }
 }
-
-// function getAllUrlParams(url) {
-
-//     // get query string from url (optional) or window
-//     var queryString = url ? url.split('#')[1] : window.location.search.slice(1);
-  
-//     // we'll store the parameters here
-//     var obj = {};
-  
-//     // if query string exists
-//     if (queryString) {
-  
-//       // stuff after # is not part of query string, so get rid of it
-//     //   queryString = queryString.split('#')[0];
-  
-//       // split our query string into its component parts
-//       var arr = queryString.split('&');
-  
-//       for (var i = 0; i < arr.length; i++) {
-//         // separate the keys and the values
-//         var a = arr[i].split('=');
-  
-//         // set parameter name and value (use 'true' if empty)
-//         var paramName = a[0];
-//         var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-  
-//         // (optional) keep case consistent
-//         // paramName = paramName.toLowerCase();
-//         // if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-  
-//         // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-//         if (paramName.match(/\[(\d+)?\]$/)) {
-  
-//           // create key if it doesn't exist
-//           var key = paramName.replace(/\[(\d+)?\]/, '');
-//           if (!obj[key]) obj[key] = [];
-  
-//           // if it's an indexed array e.g. colors[2]
-//           if (paramName.match(/\[\d+\]$/)) {
-//             // get the index value and add the entry at the appropriate position
-//             var index = /\[(\d+)\]/.exec(paramName)[1];
-//             obj[key][index] = paramValue;
-//           } else {
-//             // otherwise add the value to the end of the array
-//             obj[key].push(paramValue);
-//           }
-//         } else {
-//           // we're dealing with a string
-//           if (!obj[paramName]) {
-//             // if it doesn't exist, create property
-//             obj[paramName] = paramValue;
-//           } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-//             // if property does exist and it's a string, convert it to an array
-//             obj[paramName] = [obj[paramName]];
-//             obj[paramName].push(paramValue);
-//           } else {
-//             // otherwise add the property
-//             obj[paramName].push(paramValue);
-//           }
-//         }
-//       }
-//     }
-  
-//     return obj;
-// }
 
 async function getAgents() {
     freeze_button();
