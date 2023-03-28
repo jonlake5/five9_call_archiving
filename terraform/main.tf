268660c43e127e85d509ed1686abf4b5e1bcbf94
Jonathan Lake <jonlake5@gmail.com>
2023-03-28 07:55:56 -0400

    Update cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -722,50 +722,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn = aws_acm_certificate.app.arn
+    ssl_support_method = "sni-only"
   }
 
   price_class = "PriceClass_200"
@@ -779,13 +739,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   tags = {
     Environment = "production"
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = false
-    acm_certificate_arn = aws_acm_certificate.app.arn
-    ssl_support_method = "sni-only"
-  }
+  }  
 }
 
 resource "aws_cloudfront_origin_access_control" "default" {
